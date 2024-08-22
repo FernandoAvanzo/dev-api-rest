80f71d71388579f305bd46beb4c92bb7902d36ea
FernandoAvanzo <fernando.avanzo@gmail.com>
2024-08-22 17:17:01 -0300

    Setup

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -34,5 +34,5 @@ java {
 }
 
 application {
-    mainClass.set("AppKt")
+    mainClass.set("MainKt")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,4 +4,4 @@ plugins {
 }
 
 rootProject.name = "dev-api-rest"
-include("app")
+include("api")
